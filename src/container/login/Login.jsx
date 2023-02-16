4ecf1aa9e5d5e7ff5b04ceaf03ae5e88a70d5598
nard-calma <nard.calma@gmail.com>
2023-02-15 20:12:10 -0800

    login form created

--- src/container/login/Login.jsx
+++ src/container/login/Login.jsx
@@ -1,7 +1,20 @@
+import "./Login.css"
+
 const Login = () => {
     return(
-        <div>
-            <h1> Login </h1>
+        <div className="containerLogin">
+            <h1> LOGIN </h1>
+            <form className="formLogin">
+                <label htmlFor="username">
+                    Username: 
+                    <input type="text" name="username" placeholder="username"/>
+                </label>
+                <label htmlFor="password">
+                    Password: 
+                    <input type="password" name="password" placeholder="password"/>
+                </label>
+                <button className="btnLogin">Login</button>
+            </form>
         </div>
     )
 }
