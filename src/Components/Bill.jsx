26610b5ab4979db518d52ffc7bfff17c79b51aa4
nard-calma <nard.calma@gmail.com>
2023-02-15 19:26:24 -0800

    Fixed date format

--- src/Components/Bill.jsx
+++ src/Components/Bill.jsx
@@ -1,14 +1,19 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./Bill.css"
 
 const Bill = (props) => {
     const [runningTotal, setRunningTotal] = useState(0)
     const [target, setTarget] = useState(0)
+
+    const [date, setDate] = useState(new Date(props.bill.date).toLocaleDateString())
+    useEffect( ()=> {
+        
+    },[])
     return (
         <div className="billContainer">
-            <div className="dataContainer"><p className="billData">{props.bill.date}</p></div>
+            <div className="dataContainer"><p className="billData">{date}</p></div>
             <div className="dataContainer"><p className="billData">${props.bill.amount.toFixed(2)}</p></div>
-            <div className="dataContainer"><p className="billData">{props.bill.type}</p></div>
+            <div className="dataContainer"><p className="billData">{props.bill.expense ? "Income" : "Expense"}</p></div>
             <div className="dataContainer"><p className="billData">${runningTotal.toFixed(2)}</p></div>
             <div className="dataContainer"><p className="billData">${target.toFixed(2)}</p></div>
         </div>
--- src/container/main/Main.jsx
+++ src/container/main/Main.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 import Bill from "../../Components/Bill";
 import NavBar from "../../Components/NavBar";
 import "./Main.css"
 
 const Main = (props) => {
 
+    const [bills, setBills] = useState([])
+    const getBills = () => {
+        fetch("http://localhost:8000/plans")
+        .then((res) => res.json())
+        .then((data) => setBills(data))
+    }
+
+    const modifyDate = () => {
+        // console.log(bills.map(bill => bill))
+    }
+
+    useEffect(()=>{
+        getBills();
+        modifyDate(); 
+    }, [])
     return(
         <main className='mainContainer'>
             <NavBar 
@@ -17,7 +33,7 @@ const Main = (props) => {
                 <div className='listContainer'><h2>Target</h2></div>
             </div>
             {
-                props.bills.map((bill, index) =>
+                bills.map((bill, index) =>
                     <Bill 
                         key={index}
                         index={index}
