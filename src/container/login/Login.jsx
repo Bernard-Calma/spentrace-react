75ef1d27d7ddbbf0525886195ddc8beccbce9b64
nard-calma <nard.calma@gmail.com>
2023-02-16 18:52:38 -0800

    handleLogin created and tested

--- src/container/login/Login.jsx
+++ src/container/login/Login.jsx
@@ -11,10 +11,25 @@ const Login = () => {
         setLoginData({...loginData, [event.target.name]: event.target.value})
     }
 
+    const handleLogin = (event) => {
+        event.preventDefault();
+        fetch("http://localhost:8000/users/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(loginData)
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            console.log("data", data)
+        })
+    }
+
     return(
         <div className="containerLogin">
             <h1> LOGIN </h1>
-            <form className="formLogin" action="POST">
+            <form className="formLogin" onSubmit={handleLogin}>
                 <label htmlFor="username">
                     Username: 
                     <input type="text" name="username" placeholder="username" value={loginData.username} onChange={handleChange}/>
