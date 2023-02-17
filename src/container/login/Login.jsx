ecd5d7f026e5e9c36a6cf430f1190e56577a59d7
nard-calma <nard.calma@gmail.com>
2023-02-16 18:42:28 -0800

    added handlechange on login

--- src/container/login/Login.jsx
+++ src/container/login/Login.jsx
@@ -1,17 +1,27 @@
+import { useState } from "react";
 import "./Login.css"
 
 const Login = () => {
+    const [loginData, setLoginData] = useState({
+        username: "",
+        password: "",
+    })
+
+    const handleChange = (event) => {
+        setLoginData({...loginData, [event.target.name]: event.target.value})
+    }
+
     return(
         <div className="containerLogin">
             <h1> LOGIN </h1>
-            <form className="formLogin">
+            <form className="formLogin" action="POST">
                 <label htmlFor="username">
                     Username: 
-                    <input type="text" name="username" placeholder="username"/>
+                    <input type="text" name="username" placeholder="username" value={loginData.username} onChange={handleChange}/>
                 </label>
                 <label htmlFor="password">
                     Password: 
-                    <input type="password" name="password" placeholder="password"/>
+                    <input type="password" name="password" placeholder="password" value={loginData.password} onChange={handleChange}/>
                 </label>
                 <button className="btnLogin">Login</button>
             </form>
