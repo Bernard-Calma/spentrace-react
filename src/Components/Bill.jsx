ca0638ff32216fc1998b4be6f5fadf757f2c270c
nard-calma <nard.calma@gmail.com>
2023-02-19 10:10:23 -0800

    running total and target are added

--- src/Components/Bill.jsx
+++ src/Components/Bill.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./Bill.css"
 
 const Bill = (props) => {
@@ -6,14 +6,22 @@ const Bill = (props) => {
     const [target, setTarget] = useState(0)
 
     const [date] = useState(new Date(props.bill.date).toLocaleDateString())
+
+    const getRunningTotal = () => {
+        setRunningTotal(props.totalIncome - props.totalExpense)
+    }
+
+    useEffect(()=>{
+        getRunningTotal()
+    },[])
     return (
         <div className="billContainer">
             <div className="dataContainer"><p className="billData">{date}</p></div>
             <div className="dataContainer"><p className="billData">{props.bill.name}</p></div>
             <div className="dataContainer"><p className="billData">${props.bill.amount.toFixed(2)}</p></div>
-            <div className="dataContainer"><p className="billData">{props.bill.expense ? "Income" : "Expense"}</p></div>
-            <div className="dataContainer"><p className="billData">${runningTotal.toFixed(2)}</p></div>
-            <div className="dataContainer"><p className="billData">${target.toFixed(2)}</p></div>
+            <div className="dataContainer"><p className="billData">{props.bill.expense ? "Expense" : "Income"}</p></div>
+            <div className="dataContainer"><p className="billData">${props.bill.runningTotal.toFixed(2)}</p></div>
+            <div className="dataContainer"><p className="billData">${props.bill.target.toFixed(2)}</p></div>
         </div>
     )
 }
--- src/container/main/Main.jsx
+++ src/container/main/Main.jsx
@@ -6,21 +6,44 @@ import "./Main.css"
 const Main = (props) => {
 
     const [bills, setBills] = useState([])
-    const [totalIncome, setTotalIncome] = useState(0);
-    const [totalExpense, setTotalExpense] = useState(0);
+    let [totalIncome, setTotalIncome] = useState(0);
+    let [totalExpense, setTotalExpense] = useState(0);
+
 
     const getBills = () => {
         console.log("GetBills")
         fetch("http://localhost:8000/plans")
         .then((res) => res.json())
         .then((data) => {
-            console.log("setBills")
+            console.log("setBills", totalIncome)
+            var runningTarget = 0;
+            var total = 0;
+            for (var bill of data) {
+                if (bill.amount < 0) bill.amount = Math.abs(bill.amount) // added due to -$500 entry
+                if (bill.expense === true) {
+                    setTotalExpense(totalExpense += bill.amount)
+                    runningTarget += bill.amount;
+                    total -= Math.abs(bill.amount); // added due to -$500 entry
+                } else if (bill.expense === false) {
+                    setTotalIncome(totalIncome += bill.amount)
+                    runningTarget -= bill.amount;
+                    total += bill.amount;
+                } 
+                console.log(bill.amount)
+                if (runningTarget < 0) {
+                    bill.target = 0;
+                } else {
+                    bill.target = runningTarget;
+                }
+                bill.runningTotal = total;
+                
+            }
             setBills(data)
-            getTotalIncome(data);
-            getTotalExpense(data);
+            
         })
         
     }
+
 
     const getTotalIncome = (data) => {
         console.log("Get Total Incom")
