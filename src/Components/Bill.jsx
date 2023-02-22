e8bdb37db4bf5669b1d652a591e7587dc5091246
nard-calma <nard.calma@gmail.com>
2023-02-21 19:23:23 -0800

    changed font individual font size for bill data

--- src/Components/Bill.jsx
+++ src/Components/Bill.jsx
@@ -5,8 +5,8 @@ const Bill = (props) => {
     const [date] = useState(new Date(props.bill.date).toLocaleDateString())
     return (
         <div className="billContainer">
-            <div className="dataContainer"><p className="billData">{date}</p></div>
-            <div className="dataContainer"><p className="billData">{props.bill.name}</p></div>
+            <div className="dataContainer"><p className="billData date">{date}</p></div>
+            <div className="dataContainer"><p className="billData name">{props.bill.name}</p></div>
             <div className="dataContainer"><p className="billData">${props.bill.amount.toFixed(2)}</p></div>
             <div className="dataContainer"><p className="billData">{props.bill.expense ? "Expense" : "Income"}</p></div>
             <div className="dataContainer"><p className="billData">{props.bill.runningTotal < 0 ? "-$"+props.bill.runningTotal.toFixed(2).slice(1) : "$"+props.bill.runningTotal.toFixed(2)}</p></div>
