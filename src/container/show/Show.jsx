5c88d2bc228787ee41598f5408ed1d9dc7188464
nard-calma <nard.calma@gmail.com>
2023-02-27 01:30:04 -0800

    added onclick function to close show page

--- src/container/show/Show.jsx
+++ src/container/show/Show.jsx
@@ -5,7 +5,7 @@ const Show = (props) => {
     return(
         <div className="containerShow">
             <div className="containerShowInner">
-                <p className="closeBtn">X</p>
+                <p className="closeBtn" onClick={()=>props.handleChangeView("Main")}>X</p>
                 <p className='showInfo date'> Date: {new Date(props.openBill.date).toISOString().slice(0,10)}</p>
                 <p className='showInfo name'> Name: {props.openBill.name}</p>
                 <p className='showInfo amount'> Amount: ${props.openBill.amount.toFixed(2)}</p>
