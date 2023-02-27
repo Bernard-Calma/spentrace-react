3015a7c87f66207b08e82b3e8d3821f5358e11b3
nard-calma <nard.calma@gmail.com>
2023-02-27 01:28:47 -0800

    Added styles on show container and text tags:

--- src/container/show/Show.jsx
+++ src/container/show/Show.jsx
@@ -1,12 +1,19 @@
+import "./Show.css"
+
 const Show = (props) => {
 
     return(
-        <div>
-            <p className='addFormInput'> Date: {new Date(props.openBill.date).toISOString().slice(0,10)}</p>
-            <p className='addFormInput'> Name: {props.openBill.name}</p>
-            <p className='addFormInput'> Amount: {props.openBill.amount}</p>
-            <p className='addFormInput'> Type: {props.openBill.expense ? "Expense" : "Income"}</p>
-            <p className='addFormInput'> Notes: {props.openBill.notes}</p>
+        <div className="containerShow">
+            <div className="containerShowInner">
+                <p className="closeBtn">X</p>
+                <p className='showInfo date'> Date: {new Date(props.openBill.date).toISOString().slice(0,10)}</p>
+                <p className='showInfo name'> Name: {props.openBill.name}</p>
+                <p className='showInfo amount'> Amount: ${props.openBill.amount.toFixed(2)}</p>
+                <p className='showInfo type'> Type: {props.openBill.expense ? "Expense" : "Income"}</p>
+                <p className="showInfo">Notes: </p>
+                <p className='showInfo notes'> {props.openBill.notes}</p>
+            </div>
+            
         </div>
     )
 }
