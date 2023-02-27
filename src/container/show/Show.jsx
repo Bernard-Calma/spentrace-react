b02b06c86559ce4a2acbe14ab74fe0749e7214dd
nard-calma <nard.calma@gmail.com>
2023-02-26 16:30:40 -0800

    add bill info in show container

--- src/container/show/Show.jsx
+++ src/container/show/Show.jsx
@@ -1,8 +1,12 @@
-const Show = () => {
+const Show = (props) => {
 
     return(
         <div>
-            <h1>Show</h1>
+            <p className='addFormInput'> Date: {new Date(props.openBill.date).toISOString().slice(0,10)}</p>
+            <p className='addFormInput'> Name: {props.openBill.name}</p>
+            <p className='addFormInput'> Amount: {props.openBill.amount}</p>
+            <p className='addFormInput'> Type: {props.openBill.expense ? "Expense" : "Income"}</p>
+            <p className='addFormInput'> Notes: {props.openBill.notes}</p>
         </div>
     )
 }
