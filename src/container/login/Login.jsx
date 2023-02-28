254b5bad84655588da7a987cf73d27d3dab6861d
nard-calma <nard.calma@gmail.com>
2023-02-28 02:47:30 -0800

    add styles in login

--- src/container/login/Login.jsx
+++ src/container/login/Login.jsx
@@ -5,21 +5,14 @@ const Login = (props) => {
         <div className="containerLogin">
             <h1 className="loginTitle"> LOGIN </h1>
             <form className="formLogin" onSubmit={props.handleLogin}>
-                <label htmlFor="username">
-                    Username: 
-                    <input type="text" name="username" placeholder="username" value={props.user.username} onChange={props.handleChange}/>
-                </label>
-                <label htmlFor="password">
-                    Password: 
-                    <input type="password" name="password" placeholder="password" value={props.user.password} onChange={props.handleChange}/>
-                </label>
-                
+                <input type="text" name="username" placeholder="username" value={props.user.username} onChange={props.handleChange}/>
+                <input type="password" name="password" placeholder="password" value={props.user.password} onChange={props.handleChange}/>
                 {
                     props.loginMessage !== ""? <p className="loginMessage">{props.loginMessage}</p> : <></>
                 }
                 <button className="btnLogin">Login</button>
             </form>
-            <p onClick={() => props.handleChangeView("Register")}>Register Here</p>
+            <p className="registerText">Don't have an account yet? <span onClick={() => props.handleChangeView("Register")} className="registerText registerLink">Register Here</span></p>
         </div>
     )
 }
