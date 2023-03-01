cc31902ae1dbd0fe8f837f829a8c0759bbc108eb
nard-calma <nard.calma@gmail.com>
2023-02-28 16:52:22 -0800

    changed month date from number to string

--- src/container/show/Show.jsx
+++ src/container/show/Show.jsx
@@ -6,7 +6,7 @@ const Show = (props) => {
         <div className="containerShow">
             <p className="closeBtn" onClick={()=>props.handleChangeView("Main")}>X</p>
             <div className="containerShowInner">
-                <p className='showInfo date'> Date: {new Date(props.openBill.date).toISOString().slice(0,10)}</p>
+                <p className='showInfo date'> Date: {new Date(props.openBill.date).toString().slice(4,10)}</p>
                 <p className='showInfo name'> Name: {props.openBill.name}</p>
                 <p className='showInfo amount'> Amount: ${props.openBill.amount.toFixed(2)}</p>
                 <p className='showInfo type'> Type: {props.openBill.expense ? "Expense" : "Income"}</p>
