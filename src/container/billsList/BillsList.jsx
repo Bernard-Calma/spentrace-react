78230bd594c56be4811d6878ccdbc3ac17e1b84c
Bernard Calma <nard.calma@gmail.com>
2023-04-03 20:01:52 -0700

    added function to get total paid and unpaid

--- src/container/billsList/BillsList.jsx
+++ src/container/billsList/BillsList.jsx
@@ -10,6 +10,10 @@ const BillsList = (props) => {
     const totalExpense = 0;
     const totalIncome = 0;
 
+    // Paid
+    let [totalPaid, setTotalPaid] = useState(0);
+    let [totalUnpaid, setTotalUnpaid] = useState(0);
+
     const handleShowBill = (bill) => {
         props.setOpenBill(bill)
         props.handleChangeView("Show Bill")
@@ -26,6 +30,21 @@ const BillsList = (props) => {
         else setMonth(month += 1)
         setMonthText(monthNames[month])
     }
+
+    const getTotalPaid = () => {
+        let paid = 0;
+        let unpaid = 0;
+        props.bills.forEach(element => {
+            if (element.paid) paid += element.amount
+            else unpaid += element.amount
+        });
+        setTotalPaid(paid)
+        setTotalUnpaid(unpaid)
+    }
+
+    useEffect(()=> {
+        getTotalPaid()
+    },[])
     return(
         <section className='containerBillsList'>
             <div className='billsListHeader'>
@@ -35,19 +54,30 @@ const BillsList = (props) => {
             </div>
 
             <div className="billsContainer">
+                <div className='containerPaid'>
+                    <p>Unaid: </p>
+                    <p>${totalPaid.toFixed(2)}</p>
+                </div>
                 {
                     props.bills?.map((bill, index) => 
                         new Date(bill.dueDate).getMonth() === month ?
-                        <Bill 
-                        key={index}
-                        index={index}
-                        bill={bill}
-                        totalIncome = {totalIncome}
-                        totalExpense = {totalExpense}
-                        handleChangeView = {props.handleChangeView}
-                        handleShowBill = {handleShowBill}
-                    />: <></>)
+                        <>
+                            <Bill 
+                            key={index}
+                            index={index}
+                            bill={bill}
+                            totalIncome = {totalIncome}
+                            totalExpense = {totalExpense}
+                            handleChangeView = {props.handleChangeView}
+                            handleShowBill = {handleShowBill}
+                            />
+                        </>
+                        : <></>)
                 }
+                <div className='containerPaid'>
+                    <p>Paid: </p>
+                    <p>${totalUnpaid.toFixed(2)}</p>
+                </div>
             </div>
         </section>
     )
