8866a2ab22b5dbee0e44a9b9bdba3e49095835da
Bernard Calma <nard.calma@gmail.com>
2023-04-05 00:03:52 -0700

    register check added back and tested

--- src/container/landingPage/LandingPage.jsx
+++ src/container/landingPage/LandingPage.jsx
@@ -11,12 +11,58 @@ const LandingPage = (props) =>{
         username: "",
         password: "",
         verifyPassword: "",
-        errorMessage: "",
     })
     let [landingPageView, setLandingPageView] = useState("Login")
+    let [errorMessage, setErrorMessage] = useState("")
 
     const handleChangeUser = (event) => {
         setLoginUser({...loginUser, [event.target.name]: event.target.value})
+    }
+
+    // Register
+    const handleSubmitRegister = (e) => {
+        e.preventDefault();
+        // console.log(loginUser)
+        // USERNAME CHECK
+        let checkSpaceUserName = loginUser.username.match(" ")
+        if(checkSpaceUserName) {
+            props.clearPasswords()
+            setErrorMessage("Username must not contain any spaces.")
+            return
+        }
+        // PASSWORD CHECK
+        let verifyPasswordMatch = loginUser.password === loginUser.verifyPassword
+        let checkSpacePassword = loginUser.password.match(" ") || loginUser.verifyPassword.match(" ")
+        let checkPasswordLength = loginUser.password.length > 5
+        // console.log(checkPasswordLength)
+        if (!checkPasswordLength) {
+            props.clearPasswords()
+            setErrorMessage("Password should be at least 6 characters.")
+        } else if (checkSpacePassword) {
+            props.clearPasswords()
+            setErrorMessage("Password must not contain any spaces.")
+        } else if (!verifyPasswordMatch){
+            props.clearPasswords()
+            setErrorMessage("Password does not match.")
+        } else {
+            console.log("Register Sucessfully")
+            // fetch("http://localhost:8000/users/register", {
+            //     method: "POST",
+            //     headers: {
+            //         "Content-Type": "application/json",
+            //     },
+            //     body: JSON.stringify(loginUser)
+            // }).then(res => res.json())
+            // .then(data => {
+            //     // console.log(data)
+            //     if(data.error){
+            //         setRegisterMessage("Username is already taken")
+            //         props.clearPasswords()
+            //     } else {
+            //         setRegisterMessage("Registration Complete")
+            //     }
+            // })
+        }
     }
     return(
         <div className="containerLandingPage">
@@ -36,8 +82,10 @@ const LandingPage = (props) =>{
                 />
             :   <Register 
                     loginUser = {loginUser}
+                    errorMessage = {errorMessage}
                     handleChangeUser = {handleChangeUser}
                     setLandingPageView = {setLandingPageView}
+                    handleSubmitRegister = {handleSubmitRegister}
                     clearPasswords = {props.clearPasswords}
                 />
             }
--- src/container/landingPage/login/Register.jsx
+++ src/container/landingPage/login/Register.jsx
@@ -1,59 +1,13 @@
-import { useState } from "react";
 import BackButton from "../../../Components/Buttons/BackButton"
 
 const Register = (props) => {
-    let [registerMessage, setRegisterMessage] = useState("")
-    const handleSubmitRegister = (e) => {
-        e.preventDefault();
-        // console.log(props.user)
-        // USERNAME CHECK
-        let checkSpaceUserName = props.user.username.match(" ")
-        if(checkSpaceUserName) {
-            props.clearPasswords()
-            setRegisterMessage("Username must not contain any spaces.")
-            return
-        }
-        // PASSWORD CHECK
-        let verifyPasswordMatch = props.user.password === props.user.verifyPassword
-        let checkSpacePassword = props.user.password.match(" ") || props.user.verifyPassword.match(" ")
-        let checkPasswordLength = props.user.password.length > 5
-        // console.log(checkPasswordLength)
-        if (!checkPasswordLength) {
-            props.clearPasswords()
-            setRegisterMessage("Password should be at least 6 characters.")
-        } else if (checkSpacePassword) {
-            props.clearPasswords()
-            setRegisterMessage("Password must not contain any spaces.")
-        } else if (!verifyPasswordMatch){
-            props.clearPasswords()
-            setRegisterMessage("Password does not match.")
-        } else {
-            // console.log("Register Sucessfully")
-            fetch("http://localhost:8000/users/register", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(props.user)
-            }).then(res => res.json())
-            .then(data => {
-                // console.log(data)
-                if(data.error){
-                    setRegisterMessage("Username is already taken")
-                    props.clearPasswords()
-                } else {
-                    setRegisterMessage("Registration Complete")
-                }
-            })
-        }
-    }
     return(
         <div className="containerRegister">
             <div className="registerHeader">
                 <BackButton handleChangeView = {props.handleChangeView}/>
                 <h1 className="registerTitle">REGISTER</h1>
             </div>
-            <form className="formRegister" onSubmit={handleSubmitRegister}>
+            <form className="formRegister" onSubmit={props.handleSubmitRegister}>
                 <label htmlFor="username">
                     Username: 
                     <input type="text" name="username" placeholder="username" value={props.loginUser.username} onChange={props.handleChangeUser} required/>
@@ -67,7 +21,7 @@ const Register = (props) => {
                     <input type="password" name="verifyPassword" placeholder="verify password" value={props.loginUser.verifyPassword} onChange={props.handleChangeUser} required/>
                 </label>
                 {
-                    registerMessage !== ""? <p className="loginMessage">{registerMessage}</p> : <></>
+                    props.errorMessage !== ""? <p className="loginMessage">{props.errorMessage}</p> : <></>
                 }
                 <button className="btnLogin">Register</button>
             </form>
