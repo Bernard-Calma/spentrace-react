8ec49df8c8eedd35f83d8e4962d468f7548ed36c
Bernard Calma <nard.calma@gmail.com>
2023-04-07 23:24:19 -0700

    add first plan container created

--- src/container/home/EmptyDashboard.jsx
+++ src/container/home/EmptyDashboard.jsx
@@ -1,6 +1,9 @@
 const EmptyDashboard = () => {
-    return <div>
-        <h1>Empty</h1>
+    return <div className="containerEmptyDashboard">
+        <div className="containerEmptyPlan">
+            <h2>ADD YOUR FIRST PLAN</h2>
+            <i className="fi fi-rr-add addEmptyDashboard"></i>
+        </div>
     </div>
 }
 
