a8a6da4b0a7c1cf3efc4d85a15c79b758d082a15
Bernard Calma <nard.calma@gmail.com>
2023-04-13 00:43:53 -0700

    remove views inside app container, view is handled by user loggedin state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios'
+import { useState } from 'react';
 
 import './App.css';
 import Header from './container/header/Header';
@@ -11,8 +10,6 @@ const App = () => {
   // ------------------------------ VARIABLES ------------------------------
   // Server
   const [server] = useState(process.env.REACT_APP_SERVER_URL)
-  // View
-  const [view, setView] = useState("Login")
   // User information
   const [user, setUser] = useState({
     id: "",
@@ -21,33 +18,18 @@ const App = () => {
   })
   // ------------------------------ END OF VARIABLES ------------------------------
 
-  // ------------------------------ FUNCTIONS ------------------------------
-  // View
-  // Handle view change while navigating
-  const handleChangeView = view => setView(view)
-  // Access homeView in Home and pass to Header
-  const [appView, setAppView] = useState("")
-  const handleChangeHomeView = view => setAppView(view)
-  // ------------------------------ END OF FUNCTIONS ------------------------------
   return (
     <div className="App">
       <Header 
-        view = {view}
         user = {user}
         server = {server}
         setUser = {setUser}
-        handleChangeView = {handleChangeView}
-        handleChangeHomeView = {() => handleChangeHomeView("Home")}
       />
       {
         user.loggedIn 
         ? <Home 
             user = {user}
             server = {server}
-            appView = {appView}
-          
-            handleChangeView = {handleChangeView}
-            handleChangeHomeView = {() => handleChangeHomeView("")}
           />
         : <LandingPage 
             setUser = {setUser}
