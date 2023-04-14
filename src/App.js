6d3e20dafc2a20cea2a0914bae7519254f657cd4
Bernard Calma <nard.calma@gmail.com>
2023-04-13 23:47:42 -0700

    added checkuser at app level

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import './App.css';
 import Header from './container/header/Header';
 import Footer from './container/footer/Footer';
 import LandingPage from './container/landingPage/LandingPage';
 import Home from './container/home/Home';
+import axios from 'axios';
 
 const App = () => { 
   // ------------------------------ VARIABLES ------------------------------
@@ -27,7 +28,26 @@ const App = () => {
   // Access homeView in Home and pass to Header
   const [appView, setAppView] = useState("")
   const handleChangeHomeView = view => setAppView(view)
+  // Check if current session has user
+  const checkUser = () => {
+    axios({
+      method: "GET",
+      withCredentials: true,
+      url: server
+    })
+    .then(res => { 
+      if(res.data.passport.user) {
+        const user = res.data.passport.user
+        setUser({...user, username: user.username, loggedIn: true})
+      } else {
+        console.log(res.response.message)
+      }
+    }).catch(err => console.log("Login"))
+  }
   // ------------------------------ END OF FUNCTIONS ------------------------------
+  useEffect(() => {
+    checkUser()
+  },[])
   return (
     <div className="App">
       <Header 
