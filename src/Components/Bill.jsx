ebaa126ecf6e73f6ceb553808a843ba091738ec9
Bernard Calma <nard.calma@gmail.com>
2023-04-21 20:52:01 -0700

    fixed correct date to show

--- src/Components/Bill.jsx
+++ src/Components/Bill.jsx
@@ -6,7 +6,7 @@ const Bill = (props) => {
     const [dueDate] = useState(new Date(props.bill.dueDate))
     return(
         <div className = {props.bill.expense ? "billContainer expense" : "billContainer income"}>
-            <div className="billDataContainer date"><p className="billData date">{`${dueDate.getDate()}`}<span className="billData day">{` - ${days[dueDate.getDay()]}`}</span></p></div>
+            <div className="billDataContainer date"><p className="billData date">{`${dueDate.getUTCDate()}`}<span className="billData day">{` - ${days[dueDate.getDay()]}`}</span></p></div>
             <div className="billDataContainer name"><p className="billData name" onClick={props.handleShowBill}>{props.bill.name}</p></div>
             <div className="billDataContainer"><p className="billData amount">${props.bill.amount.toFixed(2)}</p></div>
             <div className="billDataContainer"><p className="billData paid">{props.bill.category? props.bill.category : "Add"}</p></div>
