f96e3ba91d6214336f49722ea8df2a5bb1f73662
Bernard Calma <nard.calma@gmail.com>
2023-04-24 00:24:32 -0700

    credit card added

--- src/container/accounts/AccountList.jsx
+++ src/container/accounts/AccountList.jsx
@@ -1,47 +1,60 @@
 import { useEffect, useState } from 'react'
 import './AccountList.css'
 import Account from './Account';
+import CreditCard from './CreditCard';
 
 const accountList = [
     {
         bank: "Bank of America",
-        accountNumber: 123456,
+        accountNumber: 1,
         balance: -256.23,
         type: "Checking",
-        interestRate: 0,
-        dueDate: "na",
-        minimumPayment: 0,
-        availableCredit: 0,
+        accountOpened: "",
     },
     {
         bank: "Capital One",
-        accountNumber: 1234567,
+        accountNumber: 2,
         balance: 200,
         type: "Checking",
-        interestRate: 0,
-        dueDate: "na",
-        minimumPayment: 0,
-        availableCredit: 0,
+        accountOpened: "",
     },
     {
         bank: "Bank of America",
-        accountNumber: 1354,
+        accountNumber: 3,
         balance: 50,
         type: "Savings",
-        interestRate: 0,
-        dueDate: "na",
-        minimumPayment: 0,
-        availableCredit: 0,
+        accountOpened: "",
     },
     {
         bank: "Capital One",
-        accountNumber: 681,
+        accountNumber: 4,
         balance: 100,
         type: "Savings",
-        interestRate: 0,
-        dueDate: "na",
-        minimumPayment: 0,
-        availableCredit: 0,
+        accountOpened: "",
+    },
+    {
+        bank: "Capital One",
+        accountNumber: 5,
+        type: "Credit Card",
+        accountOpened: "",
+        creditLimit: 1000,
+        balance: 500,
+        availableCredit: 500,
+        minimumPayment: 50,
+        interest: 4.99,
+        dueDate: "8"
+    },
+    {
+        bank: "Bank of America",
+        accountNumber: 6,
+        type: "Credit Card",
+        accountOpened: "",
+        creditLimit: 500,
+        balance: 300,
+        availableCredit: 200,
+        minimumPayment: 30,
+        interest: 4.99,
+        dueDate: "9"
     },
 ]
 
@@ -50,6 +63,8 @@ const AccountList = () => {
     const [accounts, setAccounts] = useState(accountList)
     const [checkingAccounts, setCheckingAccounts] = useState([]);
     const [savingsAccounts, setSavingsAccounts] = useState([])
+    const [creditCardAccounts, setCreditCardAccounts] = useState([]);
+    const [loanAccounts, setLoanAccounts] = useState([])
 
     // Functions
     const handleGetCheckingAccounts =  () => {
@@ -70,54 +85,92 @@ const AccountList = () => {
         })
         setSavingsAccounts(accountsToPush)
     }
+    const handleGetCreditCardAccounts =  () => {
+        const accountsToPush = []
+         accounts.forEach(account => {
+            if(account.type === "Credit Card") {
+                accountsToPush.push(account)
+            }
+        })
+        setCreditCardAccounts(accountsToPush)
+    }
 
     useEffect(()=>{
         handleGetCheckingAccounts()
         handleGetSavingsAccounts()
+        handleGetCreditCardAccounts()
     }, [])
     return <div className="sectionAccountList">
-        <section className='sectionChecking account'>
-            <h1>Checking Accounts</h1>
-            <div className='categoriesContainer account'>
-                <div className='listContainer account bank'><h2>Bank</h2></div>
-                <div className='listContainer account accountNumber'><h2>Account Number</h2></div>
-                <div className='listContainer account accountBalance'><h2>Balance</h2></div>
-            </div>
-            <div className='accountContainer'>
+        {
+            checkingAccounts.length > 0 &&
+            <section className='sectionChecking account'>
+                <h1>Checking Accounts</h1>
+                <div className='categoriesContainer account'>
+                    <div className='listContainer account bank'><h2>Bank</h2></div>
+                    <div className='listContainer account accountNumber'><h2>Account Number</h2></div>
+                    <div className='listContainer account accountBalance'><h2>Balance</h2></div>
+                </div>
+                <div className='accountContainer'>
+                    {
+                        checkingAccounts.map((account, index) => 
+                            <Account 
+                                key = {index}
+                                account = {account}
+                            />
+                        )
+                    }
+                </div>
+            </section>
+        }
+        {
+            savingsAccounts.length > 0 &&
+            <section className='sectionSavings account'>
+                <h1>Savings Accounts</h1>
+                <div className='categoriesContainer account'>
+                    <div className='listContainer account bank'><h2>Bank</h2></div>
+                    <div className='listContainer account accountNumber'><h2>Account Number</h2></div>
+                    <div className='listContainer account accountBalance'><h2>Balance</h2></div>
+                </div>
+                <div className='accountContainer'>
+                    {
+                        savingsAccounts.map((account, index) => 
+                            <Account 
+                                key = {index}
+                                account = {account}
+                            />
+                        )
+                    }
+                </div>
+            </section>
+        }
+        {
+            creditCardAccounts.length > 0 &&
+            <section className='sectionCredit account'>
+                <h1>Credit Card Accounts</h1>
+                <div className='categoriesContainer credit'>
+                    <div className='listContainer credit bank'><h2>Bank</h2></div>
+                    <div className='listContainer credit accountNumber'><h2>Account Number</h2></div>
+                    <div className='listContainer credit availableCredit'><h2>Available Credit</h2></div>
+                    <div className='listContainer credit creditBalance'><h2>Balance</h2></div>
+                    <div className='listContainer credit minimumPayment'><h2>Minimum Payment</h2></div>
+                    <div className='listContainer credit dueDate'><h2>Due Date</h2></div>
+                </div>
                 {
-                    checkingAccounts.map((account, index) => 
-                        <Account 
+                    creditCardAccounts.map((account, index) => 
+                        <CreditCard 
                             key = {index}
                             account = {account}
                         />
                     )
                 }
-            </div>
-        </section>
-        <section className='sectionSavings account'>
-            <h1>Savings Accounts</h1>
-            <div className='categoriesContainer account'>
-                <div className='listContainer account bank'><h2>Bank</h2></div>
-                <div className='listContainer account accountNumber'><h2>Account Number</h2></div>
-                <div className='listContainer account accountBalance'><h2>Balance</h2></div>
-            </div>
-            <div className='accountContainer'>
-                {
-                    savingsAccounts.map((account, index) => 
-                        <Account 
-                            key = {index}
-                            account = {account}
-                        />
-                    )
-                }
-            </div>
-        </section>
-        <section className='sectionCredit'>
+            </section>
+        }
+        {
+            loanAccounts.length > 0 &&
+            <section className='sectionLoan'>
 
-        </section>
-        <section className='sectionLoan'>
-
-        </section>
+            </section>
+        }
     </div>
 }
 
---
+++ src/container/accounts/CreditCard.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+
+// {
+//     bank: "Bank of America",
+//     accountNumber: 6,
+//     balance: 100,
+//     type: "Credit Card",
+//     accountOpened: "",
+//     creditLimit: 500,
+//     balance: 300,
+//     availableCredit: 200,
+//     minimumPayment: 30,
+//     interest: 4.99,
+//     dueDate: ""
+// },
+
+const CreditCard = (props) => {
+    const [account] = useState(props.account)
+    return <div className='bankContainer'>
+        <div className='listContainer credit bank'><h2>{account.bank}</h2></div>
+        <div className='listContainer credit accountNumber'><h2>{account.accountNumber}</h2></div>
+        <div className='listContainer credit availableCredit'>
+            {
+                account.availableCredit >= 0
+                ? <h2 className="positive">${account.availableCredit.toFixed(2)}</h2>
+                : <h2 className="negative">-${Math.abs(account.availableCredit.toFixed(2))}</h2>
+            }
+        </div>
+        <div className='listContainer credit creditBalance'>
+            {
+                account.balance >= 0
+                ? <h2 className="positive">${account.balance.toFixed(2)}</h2>
+                : <h2 className="negative">-${Math.abs(account.balance.toFixed(2))}</h2>
+            }
+        </div>
+        <div className='listContainer credit minimumPayment'><h2>${account.minimumPayment.toFixed(2)}</h2></div>
+        <div className='listContainer credit dueDate'><h2>{account.dueDate}</h2></div>
+    </div>
+}
+
+export default CreditCard;
