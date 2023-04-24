13994928c0d0824e5abf9fc3751afe0e1cabdf68
Bernard Calma <nard.calma@gmail.com>
2023-04-24 01:02:13 -0700

    populate loan account information

--- src/container/accounts/Loan.jsx
+++ src/container/accounts/Loan.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const Loan = (props) => {
     const [account] = useState(props.account)
-    const [loanAmount, setLoanAmount] = useState((account.loanAmount - account.balance) / account.loanAmount  * 100)
+    const [loanAmount] = useState((account.loanAmount - account.balance) / account.loanAmount  * 100)
     return <div className='bankContainer'>
         <div className='listContainer loan bank'><h2>{account.bank}</h2></div>
         <div className='listContainer loan availableCredit'><h2>${account.loanAmount.toFixed(2)}</h2></div>
