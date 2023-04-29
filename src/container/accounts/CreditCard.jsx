d63ffaca186e303824bae7fca2c46bef58745ac6
Bernard Calma <nard.calma@gmail.com>
2023-04-29 01:28:01 -0700

    fixed account view min payment error and add - for empty data

--- src/container/accounts/CreditCard.jsx
+++ src/container/accounts/CreditCard.jsx
@@ -5,17 +5,17 @@ const CreditCard = (props) => {
     const [balance] = useState(account.creditLimit - account.availableCredit)
     return <div className='bankContainer'>
         <div className='listContainer credit bank'><h2 onClick={props.handleShowAcc}>{account.bank}</h2></div>
-        <div className='listContainer credit accountNumber'><h2>{account.accountNumber}</h2></div>
-        <div className='listContainer credit availableCredit'><h2>${props.addComma(account.availableCredit)}</h2></div>
+        <div className='listContainer credit accountNumber'><h2>{account.accountNumber ? account.accountNumber : '-'}</h2></div>
+        <div className='listContainer credit availableCredit'><h2>{account.availableCredit >= 0 ? `$${props.addComma(account.availableCredit.toFixed(2))}` : `-$${Math.abs(props.addComma(account.availableCredit)).toFixed(2)}`}</h2></div>
         <div className='listContainer credit creditBalance'><h2>${props.addComma(balance)}</h2></div>
-        <div className='listContainer credit minimumPayment'><h2>${account.minPayment.toFixed(2)}</h2></div>
-        <div className='listContainer credit dueDate'><h2>{new Date(account.dueDate).getUTCDate()}</h2></div>
-        <div className='listContainer credit interest'><h2>{account.interest}%</h2></div>
+        <div className='listContainer credit minimumPayment'><h2>{account.minPayment ? `$${account.minPayment.toFixed(2)}` : '-'}</h2></div>
+        <div className='listContainer credit dueDate'><h2>{account.dueDate ? new Date(account.dueDate).getUTCDate() : '-'}</h2></div>
+        <div className='listContainer credit interest'><h2>{account.interest ? `${account.interest}%` : '-'}</h2></div>
         <div className='listContainer credit usage'>
             {
                 account.balance / account.creditLimit * 100 >= 30
-                ? <h2 className="positive">{balance / account.creditLimit * 100}%</h2>
-                : <h2 className="negative">{balance / account.creditLimit * 100}%</h2>
+                ? <h2 className="positive">{(balance / account.creditLimit * 100).toFixed(2)}%</h2>
+                : <h2 className="negative">{(balance / account.creditLimit * 100).toFixed(2)}%</h2>
             }
             </div>
     </div>
