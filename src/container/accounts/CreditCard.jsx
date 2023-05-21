0e00d70e072c7f9614eb636a1423c70e2845be76
Bernard-Calma <nard.calma@gmail.com>
2023-05-21 01:44:34 -0700

    fix(Credit Cards) Account does not update after editing
    Removed account state and directly show props.account to update component when edited

--- src/container/accounts/CreditCard.jsx
+++ src/container/accounts/CreditCard.jsx
@@ -1,22 +1,21 @@
 import { useState } from "react";
 
 const CreditCard = (props) => {
-    const [account] = useState(props.account);
-    const [balance] = useState(account.creditLimit - account.availableCredit);
+    const [balance] = useState(props.account.creditLimit - props.account.availableCredit);
     return (
         <div className='bankContainer'>
-            <h2 className="bank name" onClick={props.handleShowAcc}>{account.bank}</h2>
-            <h2 className="bank available credit">{account.availableCredit >= 0 ? `$${props.addComma(account.availableCredit.toFixed(2))}` : `-$${Math.abs(props.addComma(account.availableCredit)).toFixed(2)}`}</h2>
-            <h2 className="bank min payment">{account.minPayment ? `$${account.minPayment.toFixed(2)}` : '-'}</h2>
-            {(balance / account.creditLimit * 100) <= 30
-                ? <h2 className="bank usage positive">{(balance / account.creditLimit * 100).toFixed(2)}%</h2>
-                : <h2 className="bank usage negative">{(balance / account.creditLimit * 100).toFixed(2)}%</h2>
+            <h2 className="bank name" onClick={props.handleShowAcc}>{props.account.bank}</h2>
+            <h2 className="bank available credit">{props.account.availableCredit >= 0 ? `$${props.addComma(props.account.availableCredit.toFixed(2))}` : `-$${Math.abs(props.addComma(props.account.availableCredit)).toFixed(2)}`}</h2>
+            <h2 className="bank min payment">{props.account.minPayment ? `$${props.account.minPayment.toFixed(2)}` : '-'}</h2>
+            {(balance / props.account.creditLimit * 100) <= 30
+                ? <h2 className="bank usage positive">{(balance / props.account.creditLimit * 100).toFixed(2)}%</h2>
+                : <h2 className="bank usage negative">{(balance / props.account.creditLimit * 100).toFixed(2)}%</h2>
             }
-            <h2 className="bank full account number">{account.accNumber ? account.accNumber : '-'}</h2>
+            <h2 className="bank full account number">{props.account.accNumber ? props.account.accNumber : '-'}</h2>
             <h2 className="bank full balance">${props.addComma(balance)}</h2>
-            <h2 className="bank full due date">{account.dueDate ? `${new Date(account.dueDate).getUTCDate()}` : '-'}</h2>
-            <h2 className="bank full interest">{account.interest ? `${account.interest}%` : '-'}</h2>
-            
+            <h2 className="bank full due date">{props.account.dueDate ? `${new Date(props.account.dueDate).getUTCDate()}` : '-'}</h2>
+            <h2 className="bank full interest">{props.account.interest ? `${props.account.interest}%` : '-'}</h2>
+
         </div>
     );
 };
