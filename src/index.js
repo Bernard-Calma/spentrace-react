4a8978e5fb296392c15a01524c8dc032b15f00c3
Bernard Calma <101943213+Bernard-Calma@users.noreply.github.com>
2025-06-14 00:51:52 -0700

    feat(Styles): Variables
    Create variables scss for text colors and use it on app, index, landingPage and login

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
-import './index.css';
-import { Provider } from 'react-redux';
-import { store } from './store';
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
+import "./index.scss";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
