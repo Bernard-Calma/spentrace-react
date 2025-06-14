58b9c611855a30cbc642549673fac3eaee00ac52
Bernard Calma <101943213+Bernard-Calma@users.noreply.github.com>
2025-06-14 01:16:42 -0700

    feat(Demo): useEffect
    Change useEffect from getting data from server to localStorage

--- src/container/demo/DemoHome.jsx
+++ src/container/demo/DemoHome.jsx
@@ -1,9 +1,6 @@
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getPlans } from '../../features/planSlice'
-import { getBills } from '../../features/billSlice'
-import { getAccounts } from '../../features/accountSlice'
-import { changeView, toggleNavBar } from '../../features/viewSlice'
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { changeView, toggleNavBar } from "../../features/viewSlice";
 
 // import BillsList from '../bills/BillsList'
 // import PlanList from '../plan/PlansList'
@@ -12,109 +9,121 @@ import { changeView, toggleNavBar } from '../../features/viewSlice'
 // import AccountList from '../accounts/AccountList'
 
 // import './home.css'
-import '../../Components/NavBar.css'
-import Loading from '../../Components/Loading'
-import Icon from '../../common/Icon'
+import "../../Components/NavBar.css";
+import Loading from "../../Components/Loading";
+import Icon from "../../common/Icon";
+import { loadFromLocalStorage } from "../../features/demoSlice";
 
 const DemoHome = () => {
-    const dispatch = useDispatch();
-    // ------------------------------ VARIABLES ------------------------------
-    // Plans
-    const {planItems} = useSelector(store => store.plan)
-    // Bills
-    const {
-        billItems,
-        isLoading
-    } = useSelector(store => store.bill)
-    // Views
-    const {
-        view,
-        homeView,
-        planView,
-        billView,
-        accountView,
-        showNav
-    } = useSelector(store => store.view)
+  const dispatch = useDispatch();
+  // ------------------------------ VARIABLES ------------------------------
+  // Plans
+  const { planItems } = useSelector((store) => store.demo);
+  // Bills
+  const { billItems, isLoading } = useSelector((store) => store.bill);
+  // Views
+  const { view, homeView, planView, billView, accountView, showNav } =
+    useSelector((store) => store.view);
 
-    const hadleChangeView = view => {
-        // planView: homeView === "Plan" ? "Plan List" : planView
-        // This same login accross all cases is to keep the current view on each mainViews
-        // e.g if bill view is add if you change the main view and go back to bill view it will still be on add.
-        switch (view) {
-            case "Plan List":
-                dispatch(changeView({
-                    homeView: "Plan",
-                    planView: homeView === "Plan" ? "Plan List" : planView
-                }))
-                break;
-            case "Bills List":
-                dispatch(changeView({
-                    homeView: "Bills List",
-                    billView: homeView === "Bills List" ? "Bills List" : billView
-                }))
-                break;
-            case "Account List":
-                dispatch(changeView({
-                    homeView: "Account List",
-                    accountView: {view: homeView === "Account List" ? "Account List" : accountView.view}
-                }))
-                break;
-            default:
-                break;
-        }
-        dispatch(toggleNavBar())
+  const hadleChangeView = (view) => {
+    // planView: homeView === "Plan" ? "Plan List" : planView
+    // This same login accross all cases is to keep the current view on each mainViews
+    // e.g if bill view is add if you change the main view and go back to bill view it will still be on add.
+    switch (view) {
+      case "Plan List":
+        dispatch(
+          changeView({
+            homeView: "Plan",
+            planView: homeView === "Plan" ? "Plan List" : planView,
+          })
+        );
+        break;
+      case "Bills List":
+        dispatch(
+          changeView({
+            homeView: "Bills List",
+            billView: homeView === "Bills List" ? "Bills List" : billView,
+          })
+        );
+        break;
+      case "Account List":
+        dispatch(
+          changeView({
+            homeView: "Account List",
+            accountView: {
+              view:
+                homeView === "Account List" ? "Account List" : accountView.view,
+            },
+          })
+        );
+        break;
+      default:
+        break;
     }
-    // ------------------------------ END OF FUNCTIONS ------------------------------
-    useEffect(()=>{
-        dispatch(getPlans())
-        dispatch(getBills())
-        dispatch(getAccounts());
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
+    dispatch(toggleNavBar());
+  };
+  // ------------------------------ END OF FUNCTIONS ------------------------------
+  useEffect(() => {
+    dispatch(loadFromLocalStorage());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    return(
-       <section className="containerHome">
-        <h2>Demo</h2>
-            {
-                planItems.length === 0 
-                ? <>
-                    {/* {
+  return (
+    <section className="containerHome">
+      <h2>Demo</h2>
+      {planItems.length === 0 ? (
+        <>
+          {/* {
                         isLoading 
                         ? <Loading/>
                         : <EmptyDashboard/>
                     }  */}
-                </>
-                : <>
-                    <Icon 
-                        className="fi fi-rr-bars-staggered" 
-                        onClick={() => dispatch(toggleNavBar())} 
-                    />
-                    <div className={`homeNavBar ${showNav}`}>
-                        <p  className={`navItem ${homeView === "Plan" ? "selected" : ''}`}
-                            onClick={() => hadleChangeView("Plan List")}>Budget</p>
-                        {billItems.length > 0 && 
-                            <p className={`navItem ${homeView === "Bills List" ? "selected" : ''}`}
-                                onClick={() => hadleChangeView("Bills List")}>
-                                Bills
-                            </p>}
-                            <p  className={`navItem ${homeView === "Account List" ? "selected" : ''}`}
-                                onClick={() => hadleChangeView("Account List")}>
-                                    Accounts
-                            </p>
-                    </div>
-                    <div className='containerHomeView'>
-                        {/* {homeView === "Home" || view === "Home"
+        </>
+      ) : (
+        <>
+          <Icon
+            className="fi fi-rr-bars-staggered"
+            onClick={() => dispatch(toggleNavBar())}
+          />
+          <div className={`homeNavBar ${showNav}`}>
+            <p
+              className={`navItem ${homeView === "Plan" ? "selected" : ""}`}
+              onClick={() => hadleChangeView("Plan List")}
+            >
+              Budget
+            </p>
+            {billItems.length > 0 && (
+              <p
+                className={`navItem ${
+                  homeView === "Bills List" ? "selected" : ""
+                }`}
+                onClick={() => hadleChangeView("Bills List")}
+              >
+                Bills
+              </p>
+            )}
+            <p
+              className={`navItem ${
+                homeView === "Account List" ? "selected" : ""
+              }`}
+              onClick={() => hadleChangeView("Account List")}
+            >
+              Accounts
+            </p>
+          </div>
+          <div className="containerHomeView">
+            {/* {homeView === "Home" || view === "Home"
                             ? <DashBoard planItems = {planItems}/>
                             : homeView === "Plan" ? <PlanList/>
                             : homeView === "Bills List" ? <BillsList/>  
                             : homeView === "Account List" ? < AccountList/>  
                             : <></>
                         } */}
-                    </div>
-                </> 
-            }             
-        </section>
-    )
-}
+          </div>
+        </>
+      )}
+    </section>
+  );
+};
 
-export default DemoHome
+export default DemoHome;
--- src/features/demoSlice.js
+++ src/features/demoSlice.js
@@ -1,4 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const loadFromLocalStorage = createAsyncThunk(
+  "demo/loadFromLocalStorage",
+  async (_, thunkAPI) => {
+    try {
+      const data = localStorage.getItem("demo-state");
+      return data ? JSON.parse(data) : undefined;
+    } catch (err) {
+      console.warn("LocalStorage load failed:", err);
+      return thunkAPI.rejectWithValue("Failed to load demo state");
+    }
+  }
+);
 
 const initialState = {
   planItems: [],
@@ -14,8 +27,35 @@ const initialState = {
 const demoSlice = createSlice({
   name: "demo",
   initialState,
-  reducers: {},
+  reducers: {
+    getBudgets: (state, action) => {
+      state.planItems = action.payload;
+      state.isLoading = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(loadFromLocalStorage.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(loadFromLocalStorage.fulfilled, (state, action) => {
+        if (action.payload) {
+          state.planItems = action.payload.planItems || [];
+          state.totalIncome = action.payload.totalIncome || 0;
+          state.totalExpense = action.payload.totalExpense || 0;
+          state.balance = action.payload.balance || 0;
+          state.nextTarget = action.payload.nextTarget || {};
+          state.openPlan = action.payload.openPlan || {};
+          state.newPlanId = action.payload.newPlanId || 0;
+        }
+        state.isLoading = false;
+      })
+      .addCase(loadFromLocalStorage.rejected, (state, action) => {
+        console.warn("Failed to load demo state:", action.payload);
+        state.isLoading = false;
+      });
+  },
 });
 
-export const {} = demoSlice.actions;
+export const { getBudgets } = demoSlice.actions;
 export default demoSlice.reducer;
