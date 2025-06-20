cb0a19a984d6a2f67fcd64ebabe7d2ed447555c8
Bernard Calma <101943213+Bernard-Calma@users.noreply.github.com>
2025-06-19 20:59:02 -0700

    fix(Old UI): Old UI showing up
    Remove getUser function at app load

--- src/App.js
+++ src/App.js
@@ -1,51 +1,47 @@
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from "react-redux"
-import { getUser } from './features/userSlice';
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getUser } from "./features/userSlice";
 
-import Footer from './container/footer/Footer';
-import LandingPage from './container/landingPage/LandingPage';
-import Home from './container/home/Home';
+import Footer from "./container/footer/Footer";
+import LandingPage from "./container/landingPage/LandingPage";
+import Home from "./container/home/Home";
 
-import './app.scss';
-import DemoHome from './container/demo/DemoHome';
+import "./app.scss";
+import DemoHome from "./container/demo/DemoHome";
 
-const App = () => { 
-  const dispatch = useDispatch()
+const App = () => {
+  const dispatch = useDispatch();
   // ------------------------------ VARIABLES ------------------------------
   // User information
-  const {
-    loggedIn,
-    demo
-  } = useSelector(store => store.user)
+  const { loggedIn, demo } = useSelector((store) => store.user);
   // ------------------------------ END OF VARIABLES ------------------------------
 
   useEffect(() => {
     // Check if current session has user
-    dispatch(getUser());
+    // dispatch(getUser());
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[]);
+  }, []);
 
   return (
     <div className="App">
       {/* Render DemoHome if demo is true */}
       {/* This allows for a demo experience without requiring login */}
-      {
-        demo && <DemoHome />
-      }
+      {demo && <DemoHome />}
       {/* Render Landing Page or Home based on loggedIn state */}
-      { loggedIn
-        ? <Home/> 
-        : !demo
-          ? <LandingPage/> 
-          // If demo is true, render DemoHome instead of LandingPage
-          // This allows for a demo experience without requiring login
-          // and still provides the landing page experience for regular users
-          // who are not logged in or using the demo.
-        : <></> 
-      }
+      {loggedIn ? (
+        <Home />
+      ) : !demo ? (
+        <LandingPage />
+      ) : (
+        // If demo is true, render DemoHome instead of LandingPage
+        // This allows for a demo experience without requiring login
+        // and still provides the landing page experience for regular users
+        // who are not logged in or using the demo.
+        <></>
+      )}
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,147 +1,150 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import axios from "axios"
+import axios from "axios";
 
 const initialState = {
-    username: "",
-    loggedIn: false,
-    demo: false,
-    errorMessage: "",
-    loading: false,
-}
+  username: "",
+  loggedIn: false,
+  demo: false,
+  errorMessage: "",
+  loading: false,
+};
 
 export const getUser = createAsyncThunk("user/getUser", async (thunkAPI) => {
-    // console.log("Get User")
-    try {
-        const res = await axios({
-            method: "GET",
-            withCredentials: true,
-            url: process.env.REACT_APP_SERVER_URL
-          })
-        return res.data;
-    } catch (err) {
-        // console.log(err.response.data)
-        return thunkAPI.rejectWithValue("Error Loging In")
-    }
-})
+  // console.log("Get User")
+  try {
+    const res = await axios({
+      method: "GET",
+      withCredentials: true,
+      url: process.env.REACT_APP_SERVER_URL,
+    });
+    return res.data;
+  } catch (err) {
+    // console.log(err.response.data)
+    return thunkAPI.rejectWithValue("Error Loging In");
+  }
+});
 
-export const userLogin = createAsyncThunk("user/userLogin", async (user, thunkAPI) => {
+export const userLogin = createAsyncThunk(
+  "user/userLogin",
+  async (user, thunkAPI) => {
     // console.log("Login User", user)
     try {
-        if (!thunkAPI.getState().user.loggedIn) {
-            const res = await axios({
-                method: 'POST',
-                url: `${process.env.REACT_APP_SERVER_URL}/users/login`,
-                data: user,
-                withCredentials: true
-            })
-            return res.data.user;
-        }
-    } catch ({response}) {
-        const {message} = response.data
-        // console.log("error: ", message)
-        return thunkAPI.rejectWithValue(message)
-    }
-})
-
-export const userRegister = createAsyncThunk("user/userRegister", async (newUser, thunkAPI) => {
-    try {
+      if (!thunkAPI.getState().user.loggedIn) {
         const res = await axios({
-            method: "POST",
-            url: `${process.env.REACT_APP_SERVER_URL}/users/register`,
-            data: newUser,
-            withCredentials: true
-        })
-        return res.data.user
-    } catch (err) {
-        // console.log("Registration Error: ", err);
-        return thunkAPI.rejectWithValue(err.response.data.message)
+          method: "POST",
+          url: `${process.env.REACT_APP_SERVER_URL}/users/login`,
+          data: user,
+          withCredentials: true,
+        });
+        return res.data.user;
+      }
+    } catch ({ response }) {
+      const { message } = response.data;
+      // console.log("error: ", message)
+      return thunkAPI.rejectWithValue(message);
     }
-})
+  }
+);
+
+export const userRegister = createAsyncThunk(
+  "user/userRegister",
+  async (newUser, thunkAPI) => {
+    try {
+      const res = await axios({
+        method: "POST",
+        url: `${process.env.REACT_APP_SERVER_URL}/users/register`,
+        data: newUser,
+        withCredentials: true,
+      });
+      return res.data.user;
+    } catch (err) {
+      // console.log("Registration Error: ", err);
+      return thunkAPI.rejectWithValue(err.response.data.message);
+    }
+  }
+);
 
 const userSlice = createSlice({
-    name: "user",
-    initialState,
-    reducers: {
-        logout: state => {
-            axios(`${process.env.REACT_APP_SERVER_URL}/users/signout`)
-            // .then(res => console.log(res))
-            state.loggedIn = false;
-            state.username = "";
-        },
-        setError: (state, {payload}) => {
-            state.errorMessage = payload;
-        },
-        clearError: state => {
-            state.errorMessage = "";
-        },
-        demoLogin: state => {
-            state.username = "demoUser";
-            state.demo = true;
-            state.loading = false;
-            state.errorMessage = "";
-            // console.log("Demo Login", payload)
-        }
+  name: "user",
+  initialState,
+  reducers: {
+    logout: (state) => {
+      axios(`${process.env.REACT_APP_SERVER_URL}/users/signout`);
+      // .then(res => console.log(res))
+      state.loggedIn = false;
+      state.username = "";
     },
-    extraReducers: builder => {
-        builder
-            //Get User
-            .addCase(getUser.pending, state => {
-                state.loggedIn = false;
-                state.loading = true;
-            })
-            .addCase(getUser.fulfilled, (state, {payload}) => {
-                state.loading = false;
-                state.loggedIn = true;
-                // console.log("fulfilled Action", action)
-                state.username = payload;
-            })
-            .addCase(getUser.rejected, (state, {payload}) => {
-                state.loading = false;
-                console.log("Error:", payload)
-                state.username = "";
-                state.loggedIn = false;
-            })
-            // Login 
-            .addCase(userLogin.pending, state => {
-                state.loggedIn = false;
-                state.loading = true;
-            })
-            .addCase(userLogin.fulfilled, (state, action) => {
-                state.loading = false;
-                state.loggedIn = true;
-                // console.log("fulfilled Action", action)
-                state.username = action.payload;
-            })
-            .addCase(userLogin.rejected, (state, {payload}) => {
-                state.loading = false;
-                console.log("Error:", payload)
-                state.errorMessage = "Invalid username or password."
-                state.loggedIn = false;
-            })
-            // Register
-            .addCase(userRegister.fulfilled, (state, action) => {
-                state.loading = true;
-                state.loggedIn = false;
-                console.log("Register Action", action.payload)
-            })
-            .addCase(userRegister.rejected, (state, {payload}) => {
-                if( payload.code === 11000) {
-                    state.errorMessage = "Email is already been used."
-                } else if (payload.message === "A user with the given username is already registered"){
-                    state.errorMessage = "Username is already been used."
-                }else state.errorMessage = payload.message
-                
-                console.log("Register Rejected", payload)
-            })
-            
-    }
-})
+    setError: (state, { payload }) => {
+      state.errorMessage = payload;
+    },
+    clearError: (state) => {
+      state.errorMessage = "";
+    },
+    demoLogin: (state) => {
+      state.username = "demoUser";
+      state.demo = true;
+      state.loading = false;
+      state.errorMessage = "";
+      // console.log("Demo Login", payload)
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      //Get User
+      .addCase(getUser.pending, (state) => {
+        state.loggedIn = false;
+        state.loading = true;
+      })
+      .addCase(getUser.fulfilled, (state, { payload }) => {
+        state.loading = false;
+        state.loggedIn = true;
+        console.log("fulfilled Action", payload);
+        state.username = payload;
+      })
+      .addCase(getUser.rejected, (state, { payload }) => {
+        state.loading = false;
+        console.log("Error:", payload);
+        state.username = "";
+        state.loggedIn = false;
+      })
+      // Login
+      .addCase(userLogin.pending, (state) => {
+        state.loggedIn = false;
+        state.loading = true;
+      })
+      .addCase(userLogin.fulfilled, (state, action) => {
+        state.loading = false;
+        state.loggedIn = true;
+        // console.log("fulfilled Action", action)
+        state.username = action.payload;
+      })
+      .addCase(userLogin.rejected, (state, { payload }) => {
+        state.loading = false;
+        console.log("Error:", payload);
+        state.errorMessage = "Invalid username or password.";
+        state.loggedIn = false;
+      })
+      // Register
+      .addCase(userRegister.fulfilled, (state, action) => {
+        state.loading = true;
+        state.loggedIn = false;
+        console.log("Register Action", action.payload);
+      })
+      .addCase(userRegister.rejected, (state, { payload }) => {
+        if (payload.code === 11000) {
+          state.errorMessage = "Email is already been used.";
+        } else if (
+          payload.message ===
+          "A user with the given username is already registered"
+        ) {
+          state.errorMessage = "Username is already been used.";
+        } else state.errorMessage = payload.message;
 
-export const {
-    logout,
-    setError,
-    clearError,
-    demoLogin
-} = userSlice.actions;
+        console.log("Register Rejected", payload);
+      });
+  },
+});
+
+export const { logout, setError, clearError, demoLogin } = userSlice.actions;
 
 export default userSlice.reducer;
